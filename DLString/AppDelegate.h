82b270e438950677f54f31b7015bf08d38df00a5
bawangflower <flower258@dingtalk.com>
2016-06-28 09:32:59 +0800

    Initial Commit

---
+++ DLString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLString
+//
+//  Created by 杭州东灵 on 16/6/28.
+//  Copyright © 2016年 杭州东灵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DLString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DLString
+//
+//  Created by 杭州东灵 on 16/6/28.
+//  Copyright © 2016年 杭州东灵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
